696da4a2f096a08dab00920f567dc785f285f58a
Francisco Kellner <kellner.francisco@gmail.com>
2025-11-10 19:56:52 -0300

    nombre en rt_asso

--- vpc.tf
+++ vpc.tf
@@ -26,7 +26,7 @@ resource "aws_subnet" "public_subnets" {
   cidr_block        = cidrsubnet("10.0.0.0/16", 8, count.index)
   availability_zone = data.aws_availability_zones.available_zones.names[count.index]
 
-  map_public_ip_on_launch = false
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "terraform-pubsubnet-${count.index + 1}"
@@ -61,7 +61,7 @@ resource "aws_route_table" "public_route_table" {
 
 }
 
-resource "aws_route_table_association" "private_rt_asso" {
+resource "aws_route_table_association" "public_rt_asso" {
   count          = 2
   subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
   route_table_id = aws_route_table.public_route_table.id
